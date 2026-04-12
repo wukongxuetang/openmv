4d24731272af4220be5406f21ec2edc796c4f774
wukongxuetang <fyron@126.com>
2026-04-12 09:41:10 +0800

    Modify README text for baoziGun project
    Updated the README text to reflect the baoziGun project and its website.

--- scripts/libraries/_boot.py
+++ scripts/libraries/_boot.py
@@ -40,19 +40,9 @@ while True:
     time.sleep_ms(600)
 """
 
-readme_txt = """Thank you for supporting the OpenMV project!
-
-To download the IDE, please visit:
-https://openmv.io/pages/download
-
-For tutorials and documentation, please visit:
-http://docs.openmv.io/
-
-For technical support and projects, please visit the forums:
-http://forums.openmv.io/
-
-Please use github to report bugs and issues:
-https://github.com/openmv/openmv
+readme_txt = """Thank you for supporting the baoziGun project!
+Thank you for visting www.wukongxuetang.com/aimit!
+enjoy tracking and shooting!
 """
 
 bdev = None
@@ -102,7 +92,7 @@ except Exception:
     vfs.VfsFat.mkfs(bdev)
     fat = vfs.VfsFat(bdev)
     vfs.mount(fat, "/flash")
-    create_file("/flash/main.py", main_py)
+    #create_file("/flash/main.py", main_py)
     create_file("/flash/README.txt", readme_txt)
 
 os.chdir("/flash")
