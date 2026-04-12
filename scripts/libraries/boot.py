b75c8c218c1b47a89e6ca21f956ea85cf02fbbc5
wukongxuetang <fyron@126.com>
2026-04-12 17:55:10 +0800

    Fix sensor pixel format initialization

--- scripts/libraries/boot.py
+++ scripts/libraries/boot.py
@@ -80,9 +80,8 @@ def get_button_state():
 usb = USB_VCP()
 uart = UART(3, 115200)
 uart.init(115200, bits=8, parity=None, stop=1, flow=0)
-sensorMode = sensor.GRAYSCALE
 sensor.reset()
-sensor.set_pixformat(sensorMode)
+sensor.set_pixformat(sensor.GRAYSCALE)
 sensor.set_framesize(sensor.VGA)
 sensor.skip_frames(time=200)
 sensor.set_auto_whitebal(False, rgb_gain_db = (0.0, 0.0, 0.0))
